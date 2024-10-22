0131cdc07d3093ecfd9aa5c025c06bd27349d8f6
joeban <joeanddaneil5@gmail.com>
2024-10-22 14:23:50 +0800

    feat: Tutorial - Using Preload Scripts - 在 preload script 創建物件，使用 global 方式給 render.js 使用

--- preload.js
+++ preload.js
@@ -1,3 +1,18 @@
+const { contextBridge } = require("electron");
+
+contextBridge.exposeInMainWorld("versions", {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  // we can also expose variables, not just functions
+});
+
+contextBridge.exposeInMainWorld("constant", {
+  hello: "hello World!",
+  // we can also expose variables, not just functions
+});
+
+
 window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
--- render.js
+++ render.js
@@ -1,14 +1,9 @@
-// renderer.js
-
-// 操作 DOM 元素
-const rootElement = document.getElementById("root");
-
-// 新增內容到 #root
-const newDiv = document.createElement("div");
-newDiv.textContent = "This is rendered by renderer.js";
-rootElement.appendChild(newDiv);
-
-// 你也可以添加更多功能，比如事件處理
-document.addEventListener("click", () => {
-  alert("Document clicked!");
-});
+const information = document.getElementById("info");
+// alert("information", information);
+console.log("versions", versions);
+information.innerText = `This app is using 
+Chrome (v${versions.chrome()}), 
+Node.js (v${versions.node()}), 
+and Electron (v${versions.electron()})
+constant: hello: ${constant.hello}
+`;
