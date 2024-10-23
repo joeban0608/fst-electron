cb897c20e415d775a02c86eb69ccdfc6ef74c1fa
hongbangzhou <joeanddaneil5@gmail.com>
2024-10-23 22:47:42 +0800

    feat: send msg between renderer.js check

--- renderer.js
+++ renderer.js
@@ -1,7 +1,14 @@
-// 當電子的消息端口可用時，發送消息
-console.log('outSide here')
-if (window.electronMessagePort) {
-  console.log("here");
-  window.electronMessagePort.postMessage("ping from Main Window");
-}
-console.log('after here')
+document.addEventListener('DOMContentLoaded', () => {
+  const sendMsgBtnDom = document.getElementById("send-msg");
+  console.log("sendMsgBtnDom", sendMsgBtnDom);
+
+  // 當電子的消息端口可用時，發送消息
+  sendMsgBtnDom?.addEventListener("click", () => {
+    console.log("outSide here");
+    if (window.electronMessagePort) {
+      console.log("here");
+      window.electronMessagePort.postMessage("ping from Main Window");
+    }
+    console.log("after here");
+  });
+});
